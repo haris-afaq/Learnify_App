fcc43f5d031cbc2d4c65af8b5591e05cd69332c4
Haris <harisafaq22@gmail.com>
2026-01-07 15:51:16 +0500

    "Featured Courses and My Courses Tabs Created"

--- lib/views/explore/explore.dart
+++ lib/views/explore/explore.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:learnify_app/config/Components/app_bar.dart';
+import 'package:learnify_app/config/Components/text_styles/heading2.dart';
 import 'package:learnify_app/config/constants/colors.dart';
+import 'package:learnify_app/config/extenssions/sizedBox.dart';
+import 'package:learnify_app/views/home_screen/widgets/courses_tile.dart';
 
 class ExploreScreen extends StatefulWidget {
+
+  static const List<Tab> categoriesList = <Tab>[
+    Tab(text: 'All'),
+    Tab(text: 'Programming'),
+    Tab(text: "Designing",),
+  ];
+
   const ExploreScreen({super.key});
 
   @override
@@ -10,12 +20,95 @@ class ExploreScreen extends StatefulWidget {
 }
 
 class _ExploreScreenState extends State<ExploreScreen> {
+    TextEditingController searchController = TextEditingController();
+    @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    searchController.dispose();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.appBackgroundColor,
-      appBar: MyAppBar(title: "Featured Courses",
+    return DefaultTabController(
+  length: ExploreScreen.categoriesList.length,
+  child: Scaffold(
+    backgroundColor: AppColors.appBackgroundColor,
+    appBar: MyAppBar(
+      title: "Featured Courses",
+    ),
+    body: Padding(
+      padding: const EdgeInsets.only(top: 24, left: 15, right: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Search Field
+          TextFormField(
+            decoration: InputDecoration(
+              label: const Text("Search here..."),
+              labelStyle: TextStyle(color: AppColors.grayColor, fontSize: 13),
+              fillColor: AppColors.whiteColor,
+              filled: true,
+              border: const OutlineInputBorder(),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.whiteColor)
+              ),
+              focusedBorder:  OutlineInputBorder(
+                borderSide: BorderSide(color: AppColors.grayColor)
+              )
+            ),
+          ),
+
+          20.height,
+
+          Heading2(
+            title: "Categories",
+            titleColor: AppColors.blackColor,
+          ),
+
+          12.height,
+// Categoriessss Tabsss
+          const TabBar(
+            tabs: ExploreScreen.categoriesList,
+            indicatorColor: AppColors.blueColor,
+            labelColor: AppColors.blueColor,
+            unselectedLabelColor: AppColors.blackColor,
+          ),
+          20.height,
+
+          // Tabssss Body
+          Expanded(
+            child: TabBarView(
+              children: [
+                // All Courses Tab
+                Center(child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      CoursesTile(
+                        imagePath: "assets/images/flutter_thumbnail.png", 
+                        courseTitle: "Flutter App Development", 
+                        courseDescription: "Learn flutter app development from scratch with hands-on project, with ASIF TAJ"),
+                        10.height,
+                        CoursesTile(
+                        imagePath: "assets/images/react_thumbnail.png", 
+                        courseTitle: "Complete React course with projects | Chaye aur Code", 
+                        courseDescription: "Learn React development from scratch with hands-on project, with Chaye aur Code"),
+                        10.height,
+                    ],
+                  ),
+                )),
+                Center(child: Text("Programming Courses")),
+                Center(child: Text("Designing Courses")),
+               
+              ],
+            ),
+          ),
+        ],
       ),
-    );
+    ),
+  ),
+);
+
   }
 }
--- lib/views/my_courses/my_courses.dart
+++ lib/views/my_courses/my_courses.dart
@@ -2,7 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:learnify_app/config/Components/app_bar.dart';
 import 'package:learnify_app/config/constants/colors.dart';
 
+
+
 class MyCoursesScreen extends StatefulWidget {
+  static const List<Tab> myCoursesTab = <Tab>[
+    Tab(text: "All"),
+    Tab(text: "In Progress"),
+    Tab(text: "Completed"),
+  ];
+
   const MyCoursesScreen({super.key});
 
   @override
@@ -12,9 +20,37 @@ class MyCoursesScreen extends StatefulWidget {
 class _MyCoursesScreenState extends State<MyCoursesScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: AppColors.appBackgroundColor,
-      appBar: MyAppBar(title: "My Courses"),
+    //Default Tab Controller 
+    return DefaultTabController(
+      length: MyCoursesScreen.myCoursesTab.length,
+      child: Scaffold(
+        backgroundColor: AppColors.appBackgroundColor,
+        appBar: MyAppBar(title: "My Courses"),
+        body: Padding(
+              padding: const EdgeInsets.only(top: 24, left: 15, right: 15),
+          child: Column(
+            children: [
+              //Tab Bar
+              const TabBar(
+                tabs: MyCoursesScreen.myCoursesTab,
+                indicatorColor: AppColors.blueColor,
+                unselectedLabelColor: AppColors.blackColor,
+                labelColor: AppColors.blueColor,
+              ),
+              //Tab Body
+              Expanded(
+                child: TabBarView(
+                  children: [
+                    Center(child: Text("All Courses")),
+                    Center(child: Text("In Progress Courses")),
+                    Center(child: Text("Completed Courses")),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
